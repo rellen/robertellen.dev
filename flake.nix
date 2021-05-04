48a65d6aa8a7ab1bbf167e94cf3740e4359f4042
Robert Ellen <robert@alembic.com.au>
2021-05-05 00:35:14 +1000

    Add nix files

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "SSG Still";
+
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; };
+        };
+      in
+        {
+          devShell = import ./shell.nix { inherit pkgs nixpkgs; };
+        }
+      );
+}
