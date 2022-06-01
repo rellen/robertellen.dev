9a21240d7af46e2bd65cee585b8e2448f0004293
Robert Ellen <rellen@ieee.org>
2022-06-02 05:26:57 +1000

    Develop (#20)
    Promote changes to main

--- priv/site/assets/tailwind.config.js
+++ priv/site/assets/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
-        typography: {
+        typography: ({ theme }) => ({
             DEFAULT: {
                 css: {
                     ul: {
@@ -14,15 +14,54 @@ module.exports = {
                         }
                     },
                     blockquote: {
-                        'border-left-color': '#374151',
+                        color: theme('colors.white'),
+                    },
+                    color: theme('colors.white'),
+                    a: {
+
+                        color: theme('colors.cyan'),
+                        '&:hover': {
+                            color: theme('colors.white'),
+                        }
+                    },
+                    h2: {
+                        color: theme('colors.purple'),
+                    },
+                    h3: {
+                        color: theme('colors.purple'),
+                    },
+                    strong: {
+                        color: theme('colors.orange'),
+                    },
+                    code: {
+                        color: theme('colors.green'),
+                    },
+                    'code::before': {
+                        content: '',
+                    },
+                    'code::after': {
+                        content: '',
                     }
                 }
             }
-        }
+        })
     },
     screens: {
       sm: "600px",
       md: "900px",
+    },
+    colors: {
+        'white': '#f8f8f2',
+        'black': '#282a36',
+        'purple': '#bd93f9',
+        'green': '#50fa7b',
+        'cyan': '#8be9fd',
+        'orange': '#ffb86c',
+        'selection': '#44475a',
+    },
+    fontFamily: {
+        sans: ['Source Sans Pro', 'sans-serif'],
+        serif: ['Source Serif Pro', 'serif'],
     },
   },
   variants: {
